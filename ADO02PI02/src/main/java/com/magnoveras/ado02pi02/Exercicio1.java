4205472b431a87a4e57f20a89c9fbc43b0d2c8a0
Adalberto <Adalberto@192.168.10.21>
2017-03-01 16:21:53 -0300

    Exercicio 1 - ADO 2

--- ADO02PI02/src/main/java/com/magnoveras/ado02pi02/Exercicio1.java
+++ ADO02PI02/src/main/java/com/magnoveras/ado02pi02/Exercicio1.java
@@ -5,12 +5,39 @@
  */
 package com.magnoveras.ado02pi02;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Adalberto
  */
 public class Exercicio1 {
+    
+    public static String verifica50(float numero){
+        String resultado = "";
+        
+        if (numero >= 50.0){
+            resultado = "SUCESSO";
+        }else{
+            resultado = "ERRO";
+        }
+        
+        return resultado;
+    }
+    
+    
     public static void main(String[] args) {
-        System.out.println("Teste");
+        float numero;
+        String resultado;
+        
+        Scanner leia = new Scanner(System.in);
+        
+        
+        System.out.println("Digite um número: ");
+        numero = leia.nextFloat();
+        
+        resultado = verifica50(numero);
+        System.out.println(resultado);
+        System.out.println("FIM DO PROGRAMA");
     }
 }
